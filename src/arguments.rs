a37954af690d48dd82c96a095e8872b3499110b8
Bryan Hyland <bryan.hyland32@gmail.com>
2024-03-14 15:26:06 -0700

    Added basic add arguments

--- src/arguments.rs
+++ src/arguments.rs
@@ -4,4 +4,10 @@ use clap::{Args, arg};
 pub struct AddArgs {
     #[arg(short, long, value_name = "FOOD", required = true, help = "The food that needs to be logged")]
     food: String,
+    #[arg(short, long, value_name = "TIME", required = false, help = "The time the food was eaten.")]
+    time: String,
+    #[arg(short, long, value_name = "DATE", required = true, help = "The date the food was eaten.")]
+    date: String,
+    #[arg(short, long, value_name = "MEAL", required = true, help = "The meal the food was eaten during.")]
+    meal: String,
 }
