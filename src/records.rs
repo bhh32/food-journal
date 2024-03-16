32042760a2c72691c6c8cf092660689cef3cda01
Bryan Hyland <bryan.hyland32@gmail.com>
2024-03-16 11:56:14 -0700

    Started figuring out backend

--- src/records.rs
+++ src/records.rs
@@ -1,3 +1,8 @@
+use csv::ReaderBuilder;
+use serde::Deserialize;
+use std::error::Error;
+
+#[derive(Debug, Clone, Deserialize)]
 pub enum Meal {
     BREAKFAST,
     BRUNCH,
@@ -6,20 +11,50 @@ pub enum Meal {
     DINNER
 }
 
+#[derive(Debug, Deserialize, Clone)]
 pub struct Entry {
+    #[serde(rename = "ID")]
     id: u64,
+    #[serde(rename = "Meal")]
     meal: Meal,
+    #[serde(rename = "Food")]
     food: String,
+    #[serde(rename = "Time")]
     time: Option<String>,
+    #[serde(rename = "Date")]
     date: String,
 }
 
 impl Entry {
-    pub fn add(meal: Meal, food: String, time: Option<String>, date: String) {
-        todo!();
+    pub fn add(meal: Meal, food: String, time: Option<String>, date: String) -> Result<(), Box<dyn Error>>{
+        let mut record_container = Vec::<Entry>::new();
+
+        let journal = std::fs::File::open("food_journal.csv")?;
+        let mut rdr = ReaderBuilder::new()
+            .has_headers(true)
+            .from_reader(journal);
+
+        for result in rdr.deserialize() {
+            let record: Entry = result?;
+            record_container.push(record);
+        }
+
+        let last_id = record_container[record_container.len() - 1].id;
+
+        let new_entry: Entry = Entry {
+            id: last_id + 1,
+            meal,
+            food,
+            time,
+            date
+        };
+
+        record_container.push(new_entry);
+
+        Ok(())
     }
 
-    pub fn edit(&mut self) {
+    pub fn edit(id: u64) {
         todo!();
     }
 }
@@ -30,4 +65,8 @@ pub fn list_all() {
 
 pub fn list_range(start: String, end: String) {
     todo!();
+}
+
+pub fn list_single(id: u64) {
+
 }
